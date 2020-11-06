be682a17cd786475ea6244d915e7d7433ca7f4bf
Shao-Jie Tang <s510248@livemail.tw>
2020-11-06 19:12:47 +0800

    New project test

---
+++ Test20201106/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test20201106
+//
+//  Created by Jay on 2020/11/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
