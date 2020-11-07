698b382813cb5b0025e9d8fab31db6c471d0412b
Shao-Jie Tang <s510248@livemail.tw>
2020-11-07 23:57:50 +0800

    Add api request

---
+++ Test20201106/ViewController/Page2/Page2ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  Page2ViewController.swift
+//  Test20201106
+//
+//  Created by Jay on 2020/11/7.
+//
+
+import UIKit
+
+class Page2ViewController: UIViewController {
+
+    var page2ViewModel: Page2ViewModel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Test20201106/ViewController/Page2/Page2ViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  Page2ViewModel.swift
+//  Test20201106
+//
+//  Created by Jay on 2020/11/7.
+//
+
+import Foundation
+
+class Page2ViewModel {
+    var apiDatas:[ApiData]!
+    init(apiDatas:[ApiData]){
+        self.apiDatas = apiDatas
+    }
+}
